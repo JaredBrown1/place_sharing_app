6ef7ac895df2f82c205bcfae2573d1e710c1b3b0
Jared Brown <jaredbrown1@hotmail.com>
2022-01-05 21:03:25 -0800

    dummy data displaying

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-	return <h2>User Works!!</h2>;
+	const USERS = [
+		// {
+		// 	id: "u1",
+		// 	name: "Jared Brown",
+		// 	image: "https://m.media-amazon.com/images/I/61klqNb05nL._AC_SX679_.jpg",
+		// 	places: 3,
+		// },
+	];
+
+	return <UsersList items={USERS} />;
 };
 
 export default Users;

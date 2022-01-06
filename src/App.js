685407430b2c314967a159f96333480d042b9370
Jared Brown <jaredbrown1@hotmail.com>
2022-01-05 20:38:50 -0800

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import "./App.css";
 
+import {
+	BrowserRouter as Router,
+	Route,
+	Redirect,
+	Switch,
+} from "react-router-dom";
+
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
+
 function App() {
 	return (
-		<div className="App">
-			<h1>Lets start</h1>
-		</div>
+		<Router>
+			<Switch>
+				<Route exact path="/">
+					<Users />
+				</Route>
+				<Route exact path="/places/new">
+					<NewPlace />
+				</Route>
+				<Redirect to="/" />
+			</Switch>
+		</Router>
 	);
 }
 
